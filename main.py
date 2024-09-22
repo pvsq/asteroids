43637587370e63e58616a1163bb82b958c670118
pvsq <snekisal1v@protonmail.com>
2024-09-22 14:45:49 +0530

    Add player.py; update game code to display player

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -14,12 +15,17 @@ def main():
     # screen: pygame.Surface
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     # game loop
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill(color=(0, 0, 0))
+        player.draw(screen)
+
         pygame.display.flip()
 
         # set framerate by pausing the game loop every 1/60 s
