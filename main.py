c6a101ef170f99ccafd36597dde3d83b68965f02
pvsq <snekisal1v@protonmail.com>
2024-09-22 19:25:56 +0530

    Add asteroid.py and asteroidfield.py; update game to display a field of moving asteroids on the screen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 
 def main():
@@ -11,6 +13,7 @@ def main():
     # groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     print('Starting asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
@@ -19,8 +22,13 @@ def main():
     # screen: pygame.Surface
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # associate object classes with Groups
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     # game loop
     while (True):
